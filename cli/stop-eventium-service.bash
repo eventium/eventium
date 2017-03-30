88f5dfe11994a667482e54564fcef8d9979cc4c8
Oleg Matvejev <omatveje@gmail.com>
2017-03-29 17:35:13 -0700

    Add script to stop eventium service

--- cli/stop-eventium-service.bash
+++ cli/stop-eventium-service.bash
@@ -1,1 +1,5 @@
 # This script will stope eventium service if it is running
+if [ $(systemctl is-active eventium) = active ]; then
+  echo "Stopping eventium service...\n";
+  sudo systemctl stop eventium;
+fi;
