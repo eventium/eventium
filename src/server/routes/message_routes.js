9e4bd5c8cd0765a1fdb75a46194f32e59a809a5d
Oleg Matvejev <omatveje@gmail.com>
2017-04-03 19:20:13 -0700

    No longer need in-memory test messages

--- src/server/routes/message_routes.js
+++ src/server/routes/message_routes.js
@@ -3,8 +3,6 @@ import express from 'express';
 import models from './../models';
 
 const messageRouter = express.Router();
-// const messages = [{ uuid: 'f27f2d20-50f1-496d-984f-946a45d90514', text: 'Hello from msg 1' }, { uuid: 'f27f2d20-51f1-496d-984f-946a45d90514', text: 'Hello from msg 2' }];
-
 messageRouter.use(bodyParser.json());
 
 messageRouter.get('/events/:id/messages/', (req, res) => {
