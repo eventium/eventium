56d6c73d95e04929982e3629cbeedc6842a72a5d
enummela <enummela@sfu.ca>
2017-04-05 17:55:38 -0700

    API endpoints for updating and creating events

--- src/server/api.js
+++ src/server/api.js
@@ -2,6 +2,8 @@ import passport from 'passport';
 import { isLoggedIn, respondWithSession } from './authentication';
 
 const models = require('./models');
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
 
 const API = (app) => {
   app.use(['/api/*'], isLoggedIn);
@@ -41,6 +43,64 @@ const API = (app) => {
     models.Event.findById(id).then(instance => {
       res.json(instance.get());
     })
+  })
+
+  app.post('/api/events/', upload.single('image'), (req, res) => {
+    const start_time = req.body.start_date + ' ' + req.body.start_time + ':00';
+    const end_time = req.body.end_date + ' ' + req.body.end_time + ':00';
+    
+    const event = {
+      image: req.file.path,
+      title: req.body.title,
+      location: req.body.location,
+      address: req.body.address,
+      city: req.body.city,
+      province: req.body.province,
+      postal_code: req.body.postal_code,
+      description: req.body.description,
+      start_time: start_time,
+      end_time: end_time,
+    };
+
+    models.Event.create(event).then(
+      event => {
+        res.json(event)
+      }
+    ).catch(error => {
+      console.log(error);
+    });
+  })
+
+  app.put('/api/events/:id', upload.single('image'), (req, res) => {
+    const id = parseInt(req.params.id);
+    const start_time = req.body.start_date + ' ' + req.body.start_time + ':00';
+    const end_time = req.body.end_date + ' ' + req.body.end_time + ':00';
+
+    let event = {
+      title: req.body.title,
+      location: req.body.location,
+      address: req.body.address,
+      city: req.body.city,
+      province: req.body.province,
+      postal_code: req.body.postal_code,
+      description: req.body.description,
+      start_time: start_time,
+      end_time: end_time,
+    };
+
+    if(req.file) {
+      event.image = req.file.path;
+    }
+
+    models.Event.upsert(event).then(
+      created => {
+        models.Event.findById(id).then(instance => {
+          res.json(instance.get());
+        });
+      }
+    ).catch(error => {
+      console.log(error);
+    });
   })
 };
 
