f04d9952d728df5567669a81ee5329ececa21f2a
Tian Lin Tan <tianlint@sfu.ca>
2017-04-08 18:21:05 -0700

    Added password hashing to populateDb.js

--- src/server/bootstrap/populateDb.js
+++ src/server/bootstrap/populateDb.js
@@ -1,30 +1,15 @@
 const fs = require('fs');
 const path = require('path');
+const bcrypt = require('bcryptjs');
 const models = require('../models');
 
-models.sequelize.sync({ force: true }).then(() => {
-  const events = populateTable('./events.json', models.Event);
-  const users = populateTable('./users.json', models.User);
-
-  Promise.all([events, users]).then(() => {
-    populateTable('./messages.json', models.Message);
-    populateTable('./invites.json', models.Invite);
-    populateTable('./members.json', models.Member);
-  },
-  () => {
-    console.log('** Error: could not populate DB with messages and members.');
-  });
-});
-
-function populateTable(file, table) {
+function populateTableWithCB(file, table, cb) {
   const promise = new Promise((resolve, reject) => {
     try {
       fs.readFile(path.resolve(__dirname, file), (err, data) => {
         const contents = JSON.parse(data);
 
-        contents.forEach((object) => {
-          table.create(object);
-        });
+        contents.forEach(cb);
         resolve();
       });
     } catch (error) {
@@ -35,3 +20,31 @@ function populateTable(file, table) {
 
   return promise;
 }
+
+function populateTable(file, table) {
+  return populateTableWithCB(file, table, (object) => {
+    table.create(object);
+  });
+}
+
+function populateUserTable(file, table) {
+  return populateTableWithCB(file, table, (object) => {
+    table.create(Object.assign(object, {
+      password: bcrypt.hashSync(object.password, 10),
+    }));
+  });
+}
+
+models.sequelize.sync({ force: true }).then(() => {
+  const events = populateTable('./events.json', models.Event);
+  const users = populateUserTable('./users.json', models.User);
+
+  Promise.all([events, users]).then(() => {
+    populateTable('./messages.json', models.Message);
+    populateTable('./invites.json', models.Invite);
+    populateTable('./members.json', models.Member);
+  },
+  () => {
+    console.log('** Error: could not populate DB with messages and members.');
+  });
+});
